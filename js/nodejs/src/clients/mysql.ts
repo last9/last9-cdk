df247d41c8aab8b0cf07bd71adf4344dd654a013
Tushar Choudhari <tushar@last9.io>
2023-07-17 15:39:05 +0530

    Add the base logic to get the query

--- js/nodejs/src/clients/mysql.ts
+++ js/nodejs/src/clients/mysql.ts
@@ -2,20 +2,28 @@
 // Transaction count metrics
 // Transaction Duration metric
 import prom from 'prom-client';
-import type { createConnection } from 'mysql';
+import type { createConnection, QueryFunction } from 'mysql';
 
 const instrumentMysql = (mysqlInstance: {
   createConnection: typeof createConnection;
 }) => {
   mysqlInstance.createConnection = new Proxy(mysqlInstance.createConnection, {
-    apply: (target, props, args) => {
-      const connection = Reflect.apply(target, props, args);
+    apply: (target, prop, receiver) => {
+      const connection = Reflect.apply(target, prop, receiver);
       return new Proxy(connection, {
-        get: (target, props, args) => {
-          if (props === 'query') {
-            console.log(args);
+        get: (target, prop, receiver) => {
+          if (prop === 'query') {
+            return function (...args: Parameters<QueryFunction>) {
+              if (typeof args[0] === 'string') {
+                console.log(args[0]);
+              } else if (typeof args[0].sql === 'string') {
+                console.log(args[0].sql);
+              }
+              // @ts-ignore
+              return Reflect.apply(target[prop], this, args);
+            } as QueryFunction;
           }
-          return Reflect.get(target, props, args);
+          return Reflect.get(target, prop, receiver);
         }
       });
     }
